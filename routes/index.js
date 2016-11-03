750feeb90b26c608c59f18607052bd47b2066c84
generalmeow <rcbrown@gmail.com>
2016-11-03 15:59:25 -0700

    fixed search bar, got images to work, contiued styling

--- routes/index.js
+++ routes/index.js
@@ -23,20 +23,69 @@ const { Books, Authors, Genres, Search } = require( '../database' )
 
 const PAGE_SIZE = 10
 
-router.get( '/', function(req, res, next) {
-  const page = req.query.page || 1
-  const { search, type } = req.query
+// router.get( '/', function(req, res, next) {
+//   const page = req.query.page || 1
+//   const { search, type } = req.query
+//
+//   Books.all(PAGE_SIZE).then( books => res.render( 'index', { books } ) )
+//
+//   // const bookFetch = ( search !== undefined && type !== undefined ) ?
+//   //   getBooksSearch( search.toLowerCase(), type ) : getBooksPage
+//   //
+//   //   bookListing( bookFetch, page ).then( result => {
+//   //     res.render('index', Object.assign( {}, result, { page, search, type: (type || '').replace('by', 'by ') } ))
+//   //   })
+//   //   .catch( error => res.send({ message: error.message, error }))
+// })
 
-  Books.all(PAGE_SIZE).then( books => res.render( 'index', { books } ) )
+router.get( '/', function( request, response) {
+  const { query } = request
+  console.log( query )
 
-  // const bookFetch = ( search !== undefined && type !== undefined ) ?
-  //   getBooksSearch( search.toLowerCase(), type ) : getBooksPage
-  //
-  //   bookListing( bookFetch, page ).then( result => {
-  //     res.render('index', Object.assign( {}, result, { page, search, type: (type || '').replace('by', 'by ') } ))
-  //   })
-  //   .catch( error => res.send({ message: error.message, error }))
+  const page = query.page || 1
+  const size = query.size || 10
+  const searchBy = query.type
+  console.log(query.search)
+  console.log(typeof query.search)
+
+
+
+  if( searchBy === undefined ) {
+    Books.all( page, size )
+      .then( books => response.render( 'index', { books, page, size } ))
+      .catch( error => response.send({ error, message: error.message }))
+  } else {
+    console.log('in the switch statement', searchBy)
+    debugger
+    switch(searchBy) {
+      case 'byAuthor':
+        Search.byAuthor(query.search)
+        .then( books =>  response.render( 'index', { books, page, size } ))
+        .catch( error => response.send({ error, message: error.message }))
+        break
+      case 'byGenre':
+        Search.byGenre(query.search)
+          .then( books => response.render( 'index', { books, page, size } ))
+          .catch( error => response.send({ error, message: error.message }))
+          break
+      default:
+        console.log('searching by Title')
+        Search.byTitle(query.search)
+        .then( books => {
+          console.log(books)
+          response.render( 'index', { books, page, size } )
+        })
+        .catch( error =>{
+          console.log(error)
+          response.send({ error, message: error.message })
+        })
+
+    }
+  }
 })
+
+
+
 
 const getBooksSearch = (search, type) => page => {
   return Search[ type ]( search )
