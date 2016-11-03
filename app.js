5979864439cc1d3c7ba14704ff61b18d308e222f
generalmeow <rcbrown@gmail.com>
2016-11-03 09:18:00 -0700

    adjusted routes, got books to render on page, adjusting styling

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use('/', index);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
